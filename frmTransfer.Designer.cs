de205ac913bccb191121bb84441c6ef12d4dbc74
Tuğçe Özkök <1030521211@erciyes.edu.tr>
2024-11-28 22:18:22 +0300

    Transferler arayüzü tasarlandı.

---
+++ frmTransfer.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace StokTakipSistemiPanel
+{
+    partial class frmTransfer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader3, columnHeader4, columnHeader5, columnHeader2 });
+            listView1.Location = new Point(180, 200);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(1500, 900);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Ürün Adı";
+            columnHeader1.Width = 250;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Ürün Adedi";
+            columnHeader3.Width = 250;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Gönderilen Depo";
+            columnHeader4.Width = 250;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Transfer Tarihi";
+            columnHeader5.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Güncel stok";
+            columnHeader2.Width = 250;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(180, 136);
+            button1.Name = "button1";
+            button1.Size = new Size(250, 41);
+            button1.TabIndex = 1;
+            button1.Text = "Transfer Süreçlerini Listele";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += this.button1_Click;
+            // 
+            // frmTransfer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(listView1);
+            Name = "frmTransfer";
+            Text = "Form2";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader2;
+        private Button button1;
+    }
+}
